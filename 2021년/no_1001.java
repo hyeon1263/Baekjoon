58d62952f27604050aa6606c877f726562629450
Sanghyeon Lee <78902875+hyeon1263@users.noreply.github.com>
2021-12-28 00:52:33 +0900

    baekjoon no.1001, no.5086 java

---
+++ 2021년/no_1001.java
@@ -0,0 +1,18 @@
+// A-B
+import java.util.Scanner;
+
+public class no_1001 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		
+		int C = scanner.nextInt();
+		int D = scanner.nextInt();
+		
+		System.out.println(A-B);
+		System.out.println(C-D);
+		
+		scanner.close();
+	}
+}
